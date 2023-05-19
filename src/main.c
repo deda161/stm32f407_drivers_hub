4523df007568f82da3501040cd6df138a2194f1b
deda161 <ededemba@proton.me>
2023-05-19 22:15:36 +0300

    update gpio driver

--- src/main.c
+++ src/main.c
@@ -4,7 +4,19 @@
 
 void main()
 {
-	gpio_configure();
+	SystemInit();
+	SystemCoreClockUpdate();
+
+	SysTick_Config(SystemCoreClock / 1000);
+
+	// configure GREEN LED
+	GPIO_Config_s gpio_a = GPIO_DEFAULT_OUTPUT(GPIOD, PIN_12);
+	uint8_t res = gpio_configure(&gpio_a);
+
+	gpio_set_output_state(GPIOD, PIN_12, GPIO_State_SET);
+
+	// configure User Button
+	//gpio_configure();
 
 	while(1)
 	{
